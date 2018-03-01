e9053a5d88d2579295ede54e2b239a3c35340c50
toncelli <toncelli@WI3007178V.wind.root.it>
2018-03-01 11:59:35 +0100

    Added Apache Felix Gogo JLine Shell

---
+++ gogoJlineShell/gogoJlineShell-1.0.10.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+declare -a resources=(
+"https://search.maven.org/remotecontent?filepath=org/apache/felix/org.apache.felix.gogo.jline/1.0.10/org.apache.felix.gogo.jline-1.0.10.jar"
+)
