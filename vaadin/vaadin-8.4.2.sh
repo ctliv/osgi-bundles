a0dedd96077a9b8a68b7e9958370ae149d92efbe
ctliv <ct.livorno@gmail.com>
2018-05-29 17:54:57 +0200

    Added Vaadin 8.4.2

---
+++ vaadin/vaadin-8.4.2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -a resources=(
+"https://repo1.maven.org/maven2/org/jsoup/jsoup/1.8.3/jsoup-1.8.3.jar"
+"https://repo1.maven.org/maven2/com/vaadin/external/gentyref/1.2.0.vaadin1/gentyref-1.2.0.vaadin1.jar"
+"https://repo1.maven.org/maven2/com/vaadin/vaadin-shared/8.4.2/vaadin-shared-8.4.2.jar"
+"https://repo1.maven.org/maven2/com/vaadin/vaadin-server/8.4.2/vaadin-server-8.4.2.jar"
+"https://repo1.maven.org/maven2/com/vaadin/vaadin-osgi-integration/8.4.2/vaadin-osgi-integration-8.4.2.jar"
+"https://repo1.maven.org/maven2/com/vaadin/vaadin-client-compiled/8.4.2/vaadin-client-compiled-8.4.2.jar"
+"https://repo1.maven.org/maven2/com/vaadin/vaadin-themes/8.4.2/vaadin-themes-8.4.2.jar"
+"https://repo1.maven.org/maven2/com/vaadin/vaadin-liferay-integration/8.4.2/vaadin-liferay-integration-8.4.2.jar"
+)
