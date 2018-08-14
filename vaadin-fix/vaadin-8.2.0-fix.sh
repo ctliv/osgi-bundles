6c94137b6a55b7a9b98181e92a4e8bc939fd94d1
ctliv <ct.livorno@gmail.com>
2018-08-14 17:04:41 +0200

    Removed fix for Vaadin OSGi Integration

--- vaadin-fix/vaadin-8.2.0-fix.sh
+++ vaadin-fix/vaadin-8.2.0-fix.sh
@@ -7,7 +7,7 @@ declare -a resources=(
 "https://repo1.maven.org/maven2/com/vaadin/external/gentyref/1.2.0.vaadin1/gentyref-1.2.0.vaadin1.jar"
 "https://repo1.maven.org/maven2/com/vaadin/vaadin-shared/8.2.0/vaadin-shared-8.2.0.jar"
 "https://repo1.maven.org/maven2/com/vaadin/vaadin-server/8.2.0/vaadin-server-8.2.0.jar"
-"${basedir}/../vaadin-liferay-integration-fix/target/vaadin-osgi-integration-fix-8.2.0.jar"
+"https://repo1.maven.org/maven2/com/vaadin/vaadin-osgi-integration/8.2.0/vaadin-osgi-integration-8.2.0.jar"
 "https://repo1.maven.org/maven2/com/vaadin/vaadin-client-compiled/8.2.0/vaadin-client-compiled-8.2.0.jar"
 "https://repo1.maven.org/maven2/com/vaadin/vaadin-themes/8.2.0/vaadin-themes-8.2.0.jar"
 "${basedir}/../vaadin-liferay-integration-fix/target/vaadin-liferay-integration-fix-8.2.0.jar"
