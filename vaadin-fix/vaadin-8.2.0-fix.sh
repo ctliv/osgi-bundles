d826cfe773839cbe6a375deeae3ae60f99e7b23d
ctliv <ct.livorno@gmail.com>
2018-08-30 19:38:21 +0200

    Working...

--- vaadin-fix/vaadin-8.2.0-fix.sh
+++ vaadin-fix/vaadin-8.2.0-fix.sh
@@ -6,7 +6,7 @@ declare -a resources=(
 "https://repo1.maven.org/maven2/org/jsoup/jsoup/1.8.3/jsoup-1.8.3.jar"
 "https://repo1.maven.org/maven2/com/vaadin/external/gentyref/1.2.0.vaadin1/gentyref-1.2.0.vaadin1.jar"
 "https://repo1.maven.org/maven2/com/vaadin/vaadin-shared/8.2.0/vaadin-shared-8.2.0.jar"
-"https://repo1.maven.org/maven2/com/vaadin/vaadin-server/8.2.0/vaadin-server-8.2.0.jar"
+"${basedir}/../vaadin-server-fix/target/vaadin-server-fix-8.2.0.jar"
 "${basedir}/../vaadin-osgi-integration-fix/target/vaadin-osgi-integration-fix-8.2.0.jar"
 "https://repo1.maven.org/maven2/com/vaadin/vaadin-client-compiled/8.2.0/vaadin-client-compiled-8.2.0.jar"
 "https://repo1.maven.org/maven2/com/vaadin/vaadin-themes/8.2.0/vaadin-themes-8.2.0.jar"
---
+++ vaadin-server-fix/vaadin-server-fix-8.2.0.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+declare -a resources=(
+"target/vaadin-liferay-integration-fix-8.2.0.jar"
+)
+
