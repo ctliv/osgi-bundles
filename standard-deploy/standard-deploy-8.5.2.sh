6c992fa315a0038434de03ebcce41f155de80d39
ctliv <ct.livorno@gmail.com>
2018-10-07 19:46:57 +0200

    Added hibernate-validator-bundle

--- standard-deploy/standard-deploy-8.5.2.sh
+++ standard-deploy/standard-deploy-8.5.2.sh
@@ -3,6 +3,7 @@
 basedir=$(dirname "$0")
 
 declare -a resources=(
+"${basedir}/../hibernate-validator-bundle/get.sh 5.3.6.Final"
 "${basedir}/../vaadin/vaadin-fix.sh 8.5.2"
 "${basedir}/../autocomplete-bundle/get.sh 0.2.4"
 "${basedir}/../geolocation-bundle/get.sh 0.1.0"
