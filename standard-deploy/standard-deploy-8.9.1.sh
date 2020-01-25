e79d3dcae5879642caf5523f3644de0f407e93e3
ctliv <ct.livorno@gmail.com>
2020-01-25 10:20:35 +0100

    Added standard deploy vaadin 8.9.1

--- standard-deploy/standard-deploy-8.9.1.sh
+++ standard-deploy/standard-deploy-8.9.1.sh
@@ -4,11 +4,10 @@ basedir=$(dirname "$0")
 
 declare -a resources=(
 "${basedir}/../hibernate-validator-bundle/get.sh 5.3.6.Final"
-"${basedir}/../vaadin/vaadin-fix-8.6.3.sh"
+"${basedir}/../vaadin/vaadin-fix.sh 8.9.1"
 "${basedir}/../autocomplete-bundle/get.sh 0.2.4"
 "${basedir}/../geolocation-bundle/get.sh 0.1.0"
 "${basedir}/../formcheckbox-bundle/get.sh 1.0.2"
 "${basedir}/../report-ui-full-bundle/get.sh 1.1"
-"${basedir}/../vaadin-sliderpanel-bundle/get.sh 2.2.0"
 )
 
